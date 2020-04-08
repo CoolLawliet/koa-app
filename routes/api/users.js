a4a2fc007d25d1b14c76fb43d0430f74b121b229
nodejslhk <531549304@qq.com>
2020-04-08 23:00:15 +0800

    注册接口

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const Router = require('koa-router');
 const router = new Router();
+const bcrypt = require('bcryptjs')
 
+//引入User
+const User = require('../../models/User')
 /**
 * @route GET api/users/test
  * @desc 测试接口
@@ -11,4 +14,40 @@ router.get('/test',async ctx=>{
     ctx.body = {msg:"users works..."};
 });
 
+/**
+ * @route POST api/users/register
+ * @desc 注册接口
+ * @access 接口是公开的
+ * */
+router.post('/register',async ctx=>{
+    //存储到数据库
+    const findResult=await User.find({email:ctx.request.body.email});
+    if (findResult.length>0){
+        ctx.status=500;
+        ctx.body={email:'邮箱已被占用'};
+    }else {
+        //没查到
+        const newUser =new User({
+            name:ctx.request.body.name,
+            email:ctx.request.body.email,
+            password:ctx.request.body.password,
+        });
+
+        await bcrypt.genSalt(10,(err,salt)=>{
+            bcrypt.hash(newUser.password,salt,(err,hash)=>{
+                if (err) throw err;
+                newUser.password=hash
+            })
+        })
+        //存储到数据库
+        await newUser.save().then(user=>{
+            ctx.body=user;
+        }).catch(err=>{
+            console.log(err);
+        });
+
+        //返回json数据
+        ctx.body=newUser
+    }
+});
 module.exports = router.routes()
