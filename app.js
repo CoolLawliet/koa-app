9f86b6ea5c96c984f4186eb72cc0b4bf5d0c02f9
nodejslhk <531549304@qq.com>
2020-04-08 22:26:47 +0800

    测试接口

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
 const koa = require('koa');
 const Router = require('koa-router');
+const mongoose = require('mongoose');
 
 //实例化koa‘
-const app=new koa();
+const app = new koa();
 const router = new Router();
 
+//引入users.js
+const users = require('./routes/api/users')
+
 //路由
-router.get('/',async ctx=>{
-    ctx.body = {msg:'Hello Koa Interfaces'}
+router.get('/', async ctx => {
+    ctx.body = {msg: 'Hello Koa Interfaces'}
 });
+
+//config
+const db=require('./config/keys').mongoURI
+
+//连接数据库
+mongoose.connect(
+    db, {
+    useNewUrlParser: true
+})
+    .then(() => {
+        console.log('Mongodb Connected...');
+    }).catch(err => {
+    console.log(err);
+})
+
+//配置路由地址
+router.use('/api/users',users)
 
 //配置路由
 app.use(router.routes()).use(router.allowedMethods());
 
 const port = process.env.PORT || 5000;
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`server started on ${port}`);
 });
