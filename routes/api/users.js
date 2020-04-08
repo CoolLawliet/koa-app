e141b9388a003763ad2e4f1f9397006144f829dc
nodejslhk <531549304@qq.com>
2020-04-08 22:29:29 +0800

    测试接口

---
+++ routes/api/users.js
@@ -0,0 +1,14 @@
+const Router = require('koa-router');
+const router = new Router();
+
+/**
+* @route GET api/users/test
+ * @desc 测试接口
+ * @access 接口是公开的
+* */
+router.get('/test',async ctx=>{
+    ctx.status=200;
+    ctx.body = {msg:"users works..."};
+});
+
+module.exports = router.routes()
